fcb5ab04b082954d7c7c055693f5096e1bc6d069
Tessssssssy <ewoo97@naver.com>
2022-11-29 14:06:15 +0900

    logout toast

--- app/src/main/java/com/tessssssssy/oop_todo_list/fragment/MyPageFragment.kt
+++ app/src/main/java/com/tessssssssy/oop_todo_list/fragment/MyPageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.google.firebase.auth.FirebaseAuth
@@ -73,6 +74,7 @@ class MyPageFragment : Fragment() {
         setting.setOnClickListener {
 
             auth.signOut()
+            Toast.makeText(this, "로그아웃 성공!", Toast.LENGTH_SHORT).show()
 
             activity?.let {
                 val intent = Intent(context, IntroActivity::class.java)
